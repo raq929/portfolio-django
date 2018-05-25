a301470d25d263587d34330aaa384480622d919c
racheltstevens <racheltstevens@gmail.com>
2018-05-25 17:12:01 -0400

    Add materialize to the project

--- client/common/js/common.js
+++ client/common/js/common.js
@@ -1,6 +1,8 @@
 import classlist from 'classlist-polyfill'
 import 'element-dataset'
 import 'normalize-css/normalize.css'
+import 'materialize-css/dist/css/materialize.css'
+import 'materialize-css/dist/js/materialize.js'
 import axios from 'axios'
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
